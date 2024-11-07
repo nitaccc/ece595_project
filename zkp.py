5b2af293be17101aa383f34f36eaf024a36f187e
nitaccc <156105590+nitaccc@users.noreply.github.com>
2024-11-06 23:39:22 -0500

    Update zkp.py

--- zkp.py
+++ zkp.py
@@ -52,6 +52,51 @@ def genProof_Ei(ri, g1, g2, c, d, h, Ui, Vi, Ei, Wi, q):
     return proof
 
 
+
+def DRE_receipt(i, c, d, h, gq, q, g1, g2, s1, n1, t, m, s, n):
+    vi = input("Type 0 or 1: ")
+    vi = int(vi)
+    ri = randint(1, q-1)
+    Ui = g1^ri
+    Vi = g2^ri
+    Ei = (h^ri) * (g1^vi)
+    tmp = str(Ui) + str(Vi) + str(Ei)
+    alpha = hashlib.sha256(tmp.encode("utf-8")).hexdigest()
+    alpha = int(alpha, 16)
+    Wi = (c^ri) * (d^(ri*alpha))
+
+    Pwf = genProof_Ei(ri, g1, g2, c, d, h, Ui, Vi, Ei, Wi, q)
+    s1 = s1 + ri
+    n1 = n1 * Ui
+    Pk_s1 = genProof_s1(n1, g1, s1, q)
+
+    # first half of the receipt
+    receipt = {"id": i, "Ui": Ui, "Vi": Vi, "Ei": Ei, "Wi": Wi, "Pwf": Pwf, "Pk_s1": Pk_s1}
+
+    # check their decision
+    while True:
+        decision = input("Confirm (y/n): ")
+        # audit receipt: (i : (Ui, Vi, Ei, Wi, Pwf{Ei}, PK{s1}), (audited, ri, vi)
+        # confirm receipt: (i : (Ui, Vi, Ei, Wi, PWF{Ei}, PK{s1}), (confirmed, PK{s})
+        if decision == "y":
+            receipt["status"] = "audit"
+            receipt["ballot"] = vi
+            receipt["ri"] = ri
+            break
+        elif decision == "n":
+            receipt["status"] = "confirm"
+            t = t + vi
+            m = m + ri * alpha
+            s = s + ri
+            n = n * Ui
+            Pk_s = genProof_s1(n, g1, s, q)
+            receipt["Pk_s"] = Pk_s
+            break
+
+    return t, m, s, s1, n, n1, receipt
+
+
+
 def printReceipt(receipt):
     filename = "Receipt" + str(receipt["id"]) + ".txt"
     f = open(filename, "w")
